be350b730f250659789e53fef6c727372ed06f48
Anirudh Agarwal <anirudhagarl13@gmail.com>
2015-10-12 11:50:08 +0530

    Getting Sentiment Score

--- tweet_fetcher/src/NLP.java
+++ tweet_fetcher/src/NLP.java
@@ -1,53 +1,41 @@
+import java.util.Properties;
+import org.ejml.simple.SimpleMatrix;
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentAnnotatedTree;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
-import java.util.Properties;
 
 public class NLP {
-    
-    static StanfordCoreNLP pipeline;
-    static Properties props;
+static StanfordCoreNLP pipeline;
 
-    public static void init() 
-    {
-        System.out.println("Entry Point -1");
-         props  = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-        System.out.println("Entry Point -2");
-        pipeline = new StanfordCoreNLP(props);
-        System.out.println("Entry Point -3");
-    }
+public static void init() {
+    Properties props = new Properties();
+    props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+    pipeline = new StanfordCoreNLP(props);
+}
 
-    public static int findSentiment(String tweet) 
-    {
-        System.out.println("Entry Point 2");
-        int mainSentiment = 0;
-        if (tweet != null && tweet.length() > 0) {
-            int longest = 0;
-            Annotation annotation = pipeline.process(tweet);
-            System.out.println("Entry Point 3");
-            for (CoreMap sentence : annotation
-                    .get(CoreAnnotations.SentencesAnnotation.class))
-            {
-                Tree tree = sentence
-                        .get(SentimentCoreAnnotations.AnnotatedTree.class);
-                System.out.println("Entry Point 4");
-                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-                System.out.println("Entry Point 5");
-                String partText = sentence.toString();
-                if (partText.length() > longest) 
-                {
-                    System.out.println("Entry Point 6");
-                    mainSentiment = sentiment;
-                    System.out.println("Entry Point 7");
-                    longest = partText.length();
-                    System.out.println("Entry Point 8");
-                }
+public static int findSentiment(String tweet) {
 
+    int mainSentiment = 0;
+    if (tweet != null && tweet.length() > 0) {
+        int longest = 0;
+        Annotation annotation = pipeline.process(tweet);
+        for (CoreMap sentence : annotation
+                .get(CoreAnnotations.SentencesAnnotation.class)) {
+            Tree tree = sentence
+                    .get(SentimentAnnotatedTree.class);
+            int sentiment = RNNCoreAnnotations.getPredictedClass(tree);       
+            String partText = sentence.toString();
+            if (partText.length() > longest) {
+                mainSentiment = sentiment;
+                longest = partText.length();
             }
         }
-        return mainSentiment;
+    }
+    return mainSentiment;
     }
 }
--- tweet_fetcher/src/client.java
+++ tweet_fetcher/src/client.java
@@ -1,25 +1,30 @@
 import java.util.*;
 import java.io.*;
-
-
-public class client{
-
-    public static void main(String[] args) 
+public class client
+{
+    /*public static void main(String[] args) 
     {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter Topic & no of Tweets");
-        String topic = in.nextLine();
-        int no = in.nextInt();
-        tweetquery tweets = new tweetquery();
-        tweets.setup(no,topic);
+        //Scanner in = new Scanner(System.in);
+        //System.out.println("Enter Topic & no of Tweets");
+        //String topic = in.nextLine();
+        //int no = in.nextInt();
+       // tweetquery tweets = new tweetquery();
+       // tweets.setup(no,topic);
         //tweets.display();
         System.out.println("Entry Point 0");
         NLP.init();
-        for(String tweet : tweets.comm)
-        {
+        //for(String tweet : tweets.comm)
+        
             System.out.println("Entry Point 1");
+            //System.out.println(tweet + " : " + NLP.findSentiment(tweet));
+     }*/
+    
+    public static void main(String[] args) {
+        ArrayList<String> tweets = new ArrayList<String>();
+        tweets.add("That was good!!");
+        NLP.init();
+        for(String tweet : tweets) {
             System.out.println(tweet + " : " + NLP.findSentiment(tweet));
         }
-        
-     }
+    }
 }
